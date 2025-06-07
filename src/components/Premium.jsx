cb81511f326e847bb96299c8a38f3ca7a74d54c7
likhithsai963 <likhithsai963@gmail.com>
2025-06-07 11:18:48 +0530

    payment integration

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,29 +1,69 @@
+import axios from "axios";
 import React from "react";
+import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
+  const handleBuyClick = async (type) => {
+    const order = await axios.post(
+      BASE_URL + "/payments/create",
+      {
+        membershipType: type,
+      },
+      { withCredentials: true }
+    );
+    const { amount, keyId, currency, notes, orderId } = order.data;
+    const options = {
+      key: keyId,
+      amount,
+      currency,
+      name: "Dev Tinder",
+      description: "Connect with others",
+      order_id: orderId,
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+      },
+      theme: {
+        color: "#F37254",
+      },
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
   return (
     <div>
       <div className="flex w-full flex-col lg:flex-row">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
-         <h1 className="font-bold text-3xl"> Silver Membership</h1>
-         <ul>
+          <h1 className="font-bold text-3xl"> Silver Membership</h1>
+          <ul>
             <li> - chat with other people</li>
             <li> - 30 Requests per day</li>
             <li> - Blue Tick</li>
             <li> - 3 months</li>
-         </ul>
-         <button className="btn btn-secondary">Buy Silver</button>
+          </ul>
+          <button
+            onClick={() => handleBuyClick("silver")}
+            className="btn btn-secondary"
+          >
+            Buy Silver
+          </button>
         </div>
         <div className="divider lg:divider-horizontal">OR</div>
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
-        <h1 className="font-bold text-3xl"> Gold Membership</h1>
-         <ul>
+          <h1 className="font-bold text-3xl"> Gold Membership</h1>
+          <ul>
             <li> - chat with other people</li>
             <li> - infinite per day</li>
             <li> - Blue Tick</li>
             <li> - 6 months</li>
-         </ul>
-         <button className="btn btn-primary">Buy Gold</button>
+          </ul>
+          <button
+            onClick={() => handleBuyClick("gold")}
+            className="btn btn-primary"
+          >
+            Buy Gold
+          </button>
         </div>
       </div>
     </div>
